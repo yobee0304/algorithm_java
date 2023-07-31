60da342c0a42fd6b391bc833ebdbce2f5e3021fe
안찬영 <chan20172@gmail.com>
2023-07-31 22:44:43 +0900

    백준6064_카잉 달력 수정중

---
+++ src/boj_6064.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class boj_6064 {
+    
+    public static int lcm(int a, int b){
+        return (a * b) / gcd(a, b);
+    }
+    
+    public static int gcd(int a, int b){
+        
+        if(b == 0) return a;
+        
+        return gcd(b, a%b);
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int T, M, N, x, y;
+        T = Integer.parseInt(sc.nextLine());
+        int[] rslt = new int[T];
+        
+        for(int i=0; i<T; i++){
+            String[] inpt = sc.nextLine().split(" ");
+            M = Integer.parseInt(inpt[0]);
+            N = Integer.parseInt(inpt[1]);
+            x = Integer.parseInt(inpt[2]);
+            y = Integer.parseInt(inpt[3]);
+            
+            int lcm = lcm(M, N);
+            int nextX = x % (M+1);
+            int nextY = x;
+            
+            while(true){
+                if(nextX > lcm){
+                    rslt[i] = -1;
+                    break;
+                }
+                
+                if(nextY == y){
+                    rslt[i] = nextX;
+                    break;
+                }
+                
+                nextX += M;
+                nextY = (nextY + M) % N;
+                nextY = nextY == 0 ? N : nextY;
+            }
+        }
+        
+        for(int i=0; i<T; i++) System.out.println(rslt[i]);
+    }
+}
