6a1a271ae2da43606fdddf4dedbdf03c7204ebcf
안찬영 <chan20172@gmail.com>
2023-08-04 22:51:35 +0900

    백준15649_N과 M (1) 풀이중

---
+++ src/boj_15649.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class boj_15649 {
+    
+    public static String[] num;
+    public static int N, M;
+    public static StringBuffer sb = new StringBuffer();
+    
+    public static void dfs(String path, int now){
+        if (now == N){
+            sb.append(path.substring(0, path.length() - 1) + "\n");
+            return;
+        }
+        
+        for(int i=0; i<N; i++){
+            if(!path.contains(num[i])) dfs(path + num[i] + " ", now+1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] inpt = sc.nextLine().split(" ");
+        
+        N = Integer.parseInt(inpt[0]);
+        M = Integer.parseInt(inpt[1]);
+        
+        num = new String[N];
+        for(int i=0; i<N; i++) num[i] = Integer.toString(i + 1);
+        
+        dfs("", 0);
+        
+        System.out.println(sb.toString());
+    }
+}
