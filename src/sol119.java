98bc78c752bbafadb7a41f5dca8f0ac0f55a3fc0
안찬영 <chan20172@gmail.com>
2023-10-16 23:22:58 +0900

    LeetCode119_풀이완료

---
+++ src/sol119.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class sol119 {
+    
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> preRow = new ArrayList<>();
+        List<Integer> nowRow = new ArrayList<>();
+
+        for(int i=0; i<=rowIndex; i++) {
+            nowRow.clear();
+
+            for(int j=0; j<=i; j++) {
+                if(j == 0 || j == i)
+                    nowRow.add(1);
+                else
+                    nowRow.add(preRow.get(j-1) + preRow.get(j));
+            }
+
+            preRow.clear();
+            preRow.addAll(nowRow);
+        }
+
+        return nowRow;
+    }
+}
